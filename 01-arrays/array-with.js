db35d51ff95a35ab2b074d255746c5a44312cb3f
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-05 19:41:32 -0300

    array-with - update module

--- 01-arrays/array-with.js
+++ 01-arrays/array-with.js
@@ -1,6 +1,6 @@
 
 
-const superHeroes = [
+const state = [
     {
         id: 1,
         name: 'Batman'
@@ -19,3 +19,21 @@ const superHeroes = [
     },
 ]
 
+const index = 1
+const newName = 'Green Lantern'
+
+// const newState = state.map((hero, i) => {
+//     if (i === index) {
+//         hero.name = newName
+//     }
+
+//     return hero
+// })
+const newState = state.with(index, {
+    ...state.at(index),
+    name: newName
+})
+
+console.table(newState)
+
+console.log('El último:', state.at(-1))
