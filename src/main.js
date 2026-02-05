32b82a86a38db6d5ee30ba8821c1463babf0de2d
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-05 18:14:18 -0300

    esnext init

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+import '../01-arrays/array-with'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hola!</h1>
+    <div class="card">
+      
+    </div>
+  </div>
+`
+
+
